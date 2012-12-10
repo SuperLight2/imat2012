75d0c07b09a5a538aa9c042bea9ca5f79d458a3e
SuperLight2 <superlight89@gmail.com>
2012-12-10 14:57:39 +0400

    fix sk_learn trainer

--- sklearn/sk_trainer.py
+++ sklearn/sk_trainer.py
@@ -7,6 +7,7 @@ import numpy as np
 from sklearn.cross_validation import cross_val_score
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.cross_validation import ShuffleSplit
+import sys
 
 from auc_calcer import calc_auc
 
@@ -22,12 +23,8 @@ def add_to_result(model, X, result):
     for prediction in Y:
         index += 1
         if index not in result:
-            result[index] = None
-        if result[index] is None:
-            result[index] = prediction
-        else:
-            for j in xrange(len(result[index])):
-                result[index][j] += prediction[j]
+            result[index] = 0
+        result[index] += prediction[1]
 
 def main():
     optparser = OptionParser(usage="""
@@ -41,6 +38,9 @@ def main():
     optparser.add_option('-i', '--iterations', dest='iterations',
         type='int', default=20,
         help='build iterations trees in one forest')
+    optparser.add_option('-v', '--validate', dest='iterations',
+        type='int', default=20,
+        help='build iterations trees in one forest')
     opts, args = optparser.parse_args()
 
     train_file = args[0]
@@ -52,13 +52,13 @@ def main():
     _Y_learn = []
     _X_test = []
 
-    print "Reading Learn..."
+    print >> sys.stderr, "Reading Learn..."
     for line in open(train_file):
         rows = line.strip().split('\t')
         _Y_learn.append(int(rows[1]))
         _X_learn.append(rows[2:])
 
-    print "Reading Test..."
+    print >> sys.stderr, "Reading Test..."
     for line in open(test_file):
         rows = line.strip().split('\t')
         _X_test.append(rows[2:])
@@ -76,15 +76,15 @@ def main():
     if opts.bagging_iterations is None:
         opts.bagging_iterations = 1
 
-        print "Training"
+        print >> sys.stderr, "Training"
         model.fit(X_learn, Y_learn)
 
-        print "Learn predicting"
+        print >> sys.stderr, "Learn predicting"
         add_to_result(model, X_learn, result_on_learn)
-        print "Test predicting"
+        print >> sys.stderr, "Test predicting"
         add_to_result(model, X_test, result_on_test)
 
-        print "AUC on learn:\t", calc_auc_on_prediction(result_on_learn, Y_learn)
+        print >> sys.stderr, "AUC on learn:\t", calc_auc_on_prediction(result_on_learn, Y_learn)
     else:
         error = 0
         rs = ShuffleSplit(len(Y_learn), n_iterations=opts.bagging_iterations, test_size=0.8, random_state=1)
@@ -95,20 +95,20 @@ def main():
             X_sub_learn, X_validate, Y_sub_learn, Y_validate = [0] * 4
             X_sub_learn, X_validate, Y_sub_learn, Y_validate = X_learn[train_index], X_learn[test_index], Y_learn[train_index], Y_learn[test_index]
 
-            print "Training"
+            print >> sys.stderr, "Training"
             model.fit(X_sub_learn, Y_sub_learn)
 
-            print "Validate predicting"
+            print >> sys.stderr, "Validate predicting"
             add_to_result(model, X_validate, result_on_learn)
-            print "Test predicting"
+            print >> sys.stderr, "Test predicting"
             add_to_result(model, X_test, result_on_test)
 
             current_error = calc_auc_on_prediction(result_on_learn, Y_validate)
             error += current_error
-            print "Current AUC on validate:\t", current_error
+            print >> sys.stderr, "Current AUC on validate:\t", current_error
 
         error /= opts.bagging_iterations
-        print "Error on cross-validation:\t", error
+        print >> sys.stderr, "Error on cross-validation:\t", error
 
     f_out = open(train_result_file, "w")
     for prediction in result_on_learn:
