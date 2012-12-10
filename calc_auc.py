ee58630e498329a655f66038047fc364baea665b
SuperLight2 <superlight89@gmail.com>
2012-12-10 22:43:28 +0400

    refactoring of calc_auc

--- calc_auc.py
+++ calc_auc.py
@@ -6,33 +6,36 @@ from tools.ml_tools import calc_auc
 
 def main():
     optparser = OptionParser(usage="""
-            %prog [OPTIONS] [SESSION_ORDER_FILE ANSWER]
+            %prog [OPTIONS] [SESSION_ORDER_FILE [ANSWERS...]]
             Read classifier results from stdin and calc AUC.
             Results should be sorted in descending order of +1-class classification probability.
             If two args are given, then calculate AUC as in contest format.
             SESSION_ORDER_FILE - 1 column file with session_id`s in prediction order.
             ANSWER - 2 column file (answer, has_switch) with right asnwer.
+            if session_id not in ASNWER file, it just will be ignored (so it can be usefull for validate sets)
     """)
     opts, args = optparser.parse_args()
 
-    answers = []
+
     if len(args) == 2:
+        answers = []
         for line in sys.stdin:
             if not line.strip():
                 continue
             answers.append(int(line.strip()))
+            print calc_auc(answers)
     else:
-        right_answer = {}
-        for line in open(args[1]):
-            session, has_switch = line.strip().split('\t')
-            right_answer[session] = int(has_switch)
-        for line in open(args[0]):
-            if not line.strip():
-                continue
-            answers.append(right_answer[line.strip()])
-        if len(right_answer) != len(answers):
-            raise "The length of given files should be equal"
-    print calc_auc(answers)
+        for answer_filepath in args[1:]:
+            answers = []
+            right_answer = {}
+            for line in open(answer_filepath):
+                session, has_switch = line.strip().split('\t')
+                right_answer[session] = int(has_switch)
+            for line in open(args[0]):
+                session_id = int(line.strip())
+                if session_id in right_answer:
+                    answers.append(right_answer[session_id])
+            print "AUC on %s:\t" % answer_filepath, calc_auc(answers)
 
 if __name__ == '__main__':
     main()
