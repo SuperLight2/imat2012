b5a007a1f6cab89c156ce291e117028a76f50bf5
SuperLight2 <superlight89@gmail.com>
2012-12-18 23:05:39 +0400

    fix correlation calcer2

--- correlation_calcer.py
+++ correlation_calcer.py
@@ -1,20 +1,5 @@
 import sys
-import os
 import math
-
-def calc_correlation(filepath):
-    X = []
-    Y = []
-    for line in open(filepath):
-        s = line.strip().split('\t')
-        X.append(float(s[0]))
-        Y.append(float(s[1]))
-    X_ = 1.0 * sum(X) / len(X)
-    Y_ = 1.0 * sum(Y) / len(Y)
-    X_2 = sum((x - X_) ** 2 for x in X)
-    Y_2 = sum((y - Y_) ** 2 for y in Y)
-    C = sum ((X[i] - X_) * (Y[i] - Y_) for i in xrange(len(X)))
-    return 1.0 * C / math.sqrt(X_2 * Y_2)
 
 def main():
     description_file = sys.argv[1]
@@ -34,7 +19,6 @@ def main():
         C[i] = [0] * len(descriptions)
     lines_count = 0
 
-    print >> sys.stderr, "Calcing mean and varriance"
     for line in open(pool_file):
         lines_count += 1
         s = map(float, line.strip().split('\t'))
